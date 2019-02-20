e7d18cdd55e95fa540707289bc6755777c479075
Romeyn <romeyng@gmail.com>
2019-02-20 14:31:58 -0400

    About to implement redux

--- src/components/administrative/Administrative.jsx
+++ src/components/administrative/Administrative.jsx
@@ -1,39 +1,34 @@
 //Page with buttons to modify database tables though api
 
-import React, { Component } from 'react';
-import NewCustomerForm from './NewCustomerForm';
+import React, { Component } from "react";
+import NewCustomerForm from "./NewCustomerForm";
 class Administrative extends Component {
-    constructor(...args){
-        super(...args);
-        this.state = {newCustomerModal: false};
-    }
-    
+  constructor(...args) {
+    super(...args);
+    this.state = { newCustomerModal: false };
+  }
 
-    showFormNewCustomer=()=>{
-        this.setState({
-            newCustomerModal: true
-        });
+  showFormNewCustomer = () => {
+    this.setState({
+      newCustomerModal: true
+    });
+  };
 
-    };
-
-    render() { 
-        let modalClose = () => this.setState({ newCustomerModal: false });
-        return ( 
-            <main className="main-content bgc-grey-100">
+  render() {
+    let modalClose = () => this.setState({ newCustomerModal: false });
+    return (
+      <main className="main-content bgc-grey-100">
         <div className="mainContent">
-        <button
-              className="btn btn-lg btn-primary" 
-              onClick={this.showFormNewCustomer}
-              
-            >
-              Create Customer
-            </button>
-            <NewCustomerForm show={this.state.newCustomerModal} onHide={modalClose}/>
-            
+          <button
+            className="btn btn-lg btn-primary"
+            onClick={this.showFormNewCustomer}
+          >
+            Create Customer
+          </button>
         </div>
       </main>
-         );
-    }
+    );
+  }
 }
- 
-export default Administrative; 
+
+export default Administrative;
--- src/components/ticketing/CreateTicket.jsx
+++ src/components/ticketing/CreateTicket.jsx
@@ -1,26 +1,58 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 
-import "react-datepicker/dist/react-datepicker.css";
+//TODO: calculate meter actual within the form
+//TODO: fields to add: Status, start time, finish time, mtow
+//TODO: Lookup fields: customer name predictive,fuel,location, select customer by id
 
 import { Modal } from "react-bootstrap";
 import axios from "axios";
+import DatePicker from "react-date-picker";
 class CreateTicket extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      startDate: Date(),
+      dateCreated: new Date(),
+      dateComplete: new Date(),
       fuelLocation: "Fuel Location",
-      fuelType: "Fuel Type"
+      fuelType: "Fuel Type",
+      meterActual: 0,
+      meterBefore: 0,
+      meterAfter: 0,
+      requestQuantity: 0,
+      paymentMethod: "",
+      supervisor: "",
+      mtow: "",
+      customerID: 1,
+      userID: 1,
+      status: "done"
     };
     this.postNewFuelTicket = this.postNewFuelTicket.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.dateCreated = this.dateCreated.bind(this);
+    this.dateComplete = this.dateComplete.bind(this);
   }
   postNewFuelTicket(e) {
     e.preventDefault();
     axios
-      .post(`http://52.15.62.203:8080/`, {
-        //TODO: params here
+      .post(`http://52.15.62.203:8080/newticket/fuelticket`, {
+        fuelType: this.state.fuelType,
+        fuelLocation: this.state.fuelLocation,
+        userID: this.state.userID,
+        customerID: this.state.customerID,
+        tailNo: this.state.tailNo,
+        arrival: this.state.dateCreated,
+        required: this.state.dateComplete,
+        origin: this.state.origin,
+        destination: this.state.destination,
+        mtow: this.state.mtow,
+        request: this.state.requestQuantity,
+        pilot: this.state.pilot,
+        supervisor: this.state.supervisor,
+        paymentMethod: this.state.paymentMethod,
+        meterBefore: this.state.meterBefore,
+        meterAfter: this.state.meterAfter,
+        status: this.state.status
       })
       .then(response => {
         console.log(response);
@@ -42,6 +74,13 @@ class CreateTicket extends Component {
       [name]: value
     });
   }
+  dateCreated(date) {
+    this.setState({ dateCreated: date });
+  }
+  dateComplete(date) {
+    this.setState({ dateComplete: date });
+  }
+  componentDidUpdate(prevProps, prevState) {}
 
   render() {
     return (
@@ -69,7 +108,13 @@ class CreateTicket extends Component {
                         className="form-control"
                         id="customerName"
                         placeholder="Flight/ Customer Name"
+                        name="customerName"
+                        onChange={this.handleChange}
+                        readOnly
                       />
+                    </div>
+                    <div className="input-group-append">
+                      <button className="ti-search form-control form-control-lg btn btn-info " />
                     </div>
                   </div>
                   <div className="form-row">
@@ -79,6 +124,7 @@ class CreateTicket extends Component {
                         className="form-control"
                         id="customerID"
                         placeholder="ID"
+                        name="customerID"
                       />
                     </div>
                     <div className="form-group col">
@@ -87,6 +133,8 @@ class CreateTicket extends Component {
                         className="form-control"
                         id="tailNumnber"
                         placeholder="Tail #"
+                        name="tailNo"
+                        onChange={this.handleChange}
                       />
                     </div>
                   </div>
@@ -97,6 +145,8 @@ class CreateTicket extends Component {
                         className="form-control"
                         id="origin"
                         placeholder="Origin"
+                        name="origin"
+                        onChange={this.handleChange}
                       />
 
                       <input
@@ -104,22 +154,39 @@ class CreateTicket extends Component {
                         className="form-control"
                         id="destination"
                         placeholder="Destination"
+                        name="destination"
+                        onChange={this.handleChange}
                       />
                     </div>
 
                     <div className="form-group col-7 p-2 ">
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="dateCreated"
-                        placeholder="Created on"
+                      <DatePicker
+                        onChange={this.dateCreated}
+                        value={this.state.dateCreated}
+                        name="dateCreated"
+                        className="mb-1"
                       />
 
+                      <DatePicker
+                        onChange={this.dateComplete}
+                        value={this.state.dateComplete}
+                        name="dateComplete"
+                        className="mt-1"
+                      />
+                    </div>
+                  </div>
+                  <div className="form-row">
+                    <div className="form-control col-3 border-0">
+                      Pilot Name
+                    </div>
+                    <div className="form-group col-9">
                       <input
                         type="text"
                         className="form-control"
-                        id="dateComplete"
-                        placeholder="Completed on"
+                        id="pilot"
+                        placeholder=""
+                        name="pilot"
+                        onChange={this.handleChange}
                       />
                     </div>
                   </div>
@@ -138,10 +205,10 @@ class CreateTicket extends Component {
                         onChange={this.handleChange}
                       >
                         <option value="0">Fuel Location</option>
-                        <option value="CIW">CIW</option>
-                        <option value="SVD">SVD</option>
-                        <option value="BGI">BGI</option>
-                        <option value="UVF">UVF</option>
+                        <option value="1">CIW</option>
+                        <option value="2">SVD</option>
+                        <option value="3">BGI</option>
+                        <option value="4">UVF</option>
                       </select>
                     </div>
                     <div className="form-group col-6 mx-auto">
@@ -166,8 +233,10 @@ class CreateTicket extends Component {
                       <input
                         type="text"
                         className="form-control form-control-lg"
-                        id="quantity"
+                        id="requestQuantity"
                         placeholder="0"
+                        name="requestQuantity"
+                        onChange={this.handleChange}
                       />
                       <div className="input-group-append">
                         <span className="input-group-text">us. gal</span>
@@ -182,6 +251,8 @@ class CreateTicket extends Component {
                         className="form-control form-control-lg"
                         type="text"
                         placeholder="Before"
+                        name="meterBefore"
+                        onChange={this.handleChange}
                       />
                       <div className="input-group-append">
                         <span className="input-group-text">us. gal</span>
@@ -193,6 +264,8 @@ class CreateTicket extends Component {
                         className="form-control form-control-lg"
                         type="text"
                         placeholder="After"
+                        name="meterAfter"
+                        onChange={this.handleChange}
                       />
                       <div className="input-group-append">
                         <span className="input-group-text">us. gal</span>
@@ -212,7 +285,7 @@ class CreateTicket extends Component {
                         name="meterActual"
                         id="meterActual"
                       >
-                        0
+                        {this.state.meterActual}
                       </div>
                       <div className="input-group-append">
                         <span className="input-group-text">us. gal</span>
@@ -236,17 +309,14 @@ class CreateTicket extends Component {
                           value="cash"
                         />
                         <label htmlFor="chkCash">Cash</label>
-                      </div>
-                      <div className="form-check form-check-inline">
+
                         <input
                           className="form-check-input"
                           type="radio"
                           id="chkCheque"
                           value="cheque"
                         />
-                        <label htmlFor="chkCheque">Cheque</label>
-                      </div>
-                      <div className="form-check form-check-inline">
+
                         <input
                           className="form-check-input"
                           type="radio"
@@ -254,7 +324,10 @@ class CreateTicket extends Component {
                           value="card"
                         />
                         <label htmlFor="chkCard">Card</label>
+                        <label htmlFor="chkCheque">Cheque</label>
                       </div>
+                      <div className="form-check form-check-inline" />
+                      <div className="form-check form-check-inline" />
                     </div>
                   </div>
                 </div>
