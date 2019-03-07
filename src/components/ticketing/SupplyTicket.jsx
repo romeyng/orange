27388bba62cb7bc6c642346baab5661feb79884f
Romeyn <romeyng@gmail.com>
2019-03-07 19:02:46 -0400

    creating supply ticket

--- src/components/ticketing/SupplyTicket.jsx
+++ src/components/ticketing/SupplyTicket.jsx
@@ -1,3 +1,48 @@
 import React, { Component } from "react";
+import { Modal } from "react-bootstrap";
 
-export const SupplyTicket = props => {};
+export const SupplyTicket = props => {
+  let supplyForm = (
+    <Modal
+      {...props}
+      size="sm"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add Supply Ticket
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <form>
+          <div className="bgc-white bd m-3 p-4">
+            <div className="row">
+              <div className="col">
+                <label htmlFor="supplier">Supplier</label>
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  id="supplier"
+                  name="supplier"
+                  onChange={props.handleChange}
+                />
+                <label htmlFor="quantity">Quantity</label>
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  id="quantity"
+                  name="quantity"
+                  onChange={props.handleChange}
+                />
+              </div>
+            </div>
+          </div>
+
+          <button className="btn btn-primary btn-lg">Submit</button>
+        </form>
+      </Modal.Body>
+    </Modal>
+  );
+  return supplyForm;
+};
--- src/components/ticketing/TicketingButtons.jsx
+++ src/components/ticketing/TicketingButtons.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 
 import CreateTicket from "./CreateTicket";
+import { SupplyTicket } from "./SupplyTicket";
 class TicketingButtons extends Component {
   constructor(...args) {
     super(...args);
 
     this.state = { modalShow: false };
+    this.state = { showSupplyForm: false };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    //function changes state as form values are editted
+
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    if (target.type == "checkbox") {
+      this.setState({ [name]: target.checked });
+    } else {
+      this.setState({
+        [name]: value
+      });
+    }
   }
 
   createTicketForm = () => {
@@ -13,8 +32,13 @@ class TicketingButtons extends Component {
       modalShow: true
     });
   };
+
+  showSupplyForm = () => {
+    this.setState({ showSupplyForm: true });
+  };
   render() {
-    let modalClose = () => this.setState({ modalShow: false });
+    let modalClose = () =>
+      this.setState({ modalShow: false, showSupplyForm: false });
     return (
       <div className="container-fluid">
         <div className="row">
@@ -27,11 +51,19 @@ class TicketingButtons extends Component {
             </button>
           </div>
           <div className="p-3">
-            <button className="btn btn-lg btn-warning">
+            <button
+              className="btn btn-lg btn-warning"
+              onClick={this.showSupplyForm}
+            >
               New Supply Ticket
             </button>
           </div>
           <CreateTicket show={this.state.modalShow} onHide={modalClose} />
+          <SupplyTicket
+            show={this.state.showSupplyForm}
+            onChange={this.handleChange}
+            onHide={modalClose}
+          />
         </div>
       </div>
     );
